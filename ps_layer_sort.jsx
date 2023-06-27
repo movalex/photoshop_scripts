5a266e46e4ef3dc83124d9190c0de8bc5f311bc3
Alexey Bogomolov <mail@abogomolov.com>
2023-06-28 00:10:23 +0300

    cleanup and formatting

--- ps_layer_sort.jsx
+++ ps_layer_sort.jsx
@@ -1,4 +1,4 @@
-#target photoshop
+// #target photoshop
 
 /**
  * @copyright Copyright (c) 2020 Matthew Kimber. All rights reserved.
@@ -14,84 +14,95 @@
 	} else {
 		alert("Please open a document to run this script.");
 	}
-
 	/**
-	 * @desc Program entry point. Retrieves the active document, determines if layers exist, and then sorts all ArtLayer and LayerSet objects.
+	 * @desc Program entry point. Retrieves the active document, 
+	 * determines if layers exist, and then sorts all ArtLayer and LayerSet objects.
 	 */
+
+	var isGroup = false
+	var currentGroup = null;
 	function main() {
-		var selectedLayers = SelectedLayers();
+		var selectedLayers = getSelectedLayers();
+		if (selectedLayers.length == 1) {
+			alert("Select 2 or more Layers or a single Group of Layers to sort", "Warning")
+			return
+		}
 		sortLayers(selectedLayers);
 	}
 
-	function SelectedLayers() {
+	function addLayerSetContents(layerSet) {
+		var targetArray = new Array();
+		var refLength = layerSet.layers.length;
+		for(var j = 0; j < refLength; j++) {
+			var refLay = layerSet.layers[j];
+			if (refLay.allLocked == true){
+				targetArray.push(refLay)}
+		}
+		return targetArray
+	}
+
+	function getSelectedLayers() {
 		try{
 			//Version V2 works with folders aka layersets
-			var ActLay = app.activeDocument.activeLayer;
-			ActLay.allLocked = true;
-			var L = app.activeDocument.layers.length;
-			var selLayers = new Array();
-			for(var i = 0; i < L; i++) {
-				var LayerType = app.activeDocument.layers[i].typename;
-				var layerRef = app.activeDocument.layers[i];
-	
-				if (LayerType == "LayerSet" ) {
-					var refLength = layerRef.layers.length;
-					for(var j = 0; j < refLength; j++) {
-						var refLay = layerRef.layers[j];
-						if (refLay.allLocked == true){selLayers.push(refLay)}
-					}
-					continue;
-				}
-				if (layerRef.allLocked == true) {selLayers.push(layerRef)}
+			var layersCount = app.activeDocument.layers.length;
+			if (layersCount == 0) {
+				alert("No Layers in the Document", "Warning")
+				return
 			}
-	
-			ActLay.allLocked = false;
-			return selLayers;
+			var activeLayer = app.activeDocument.activeLayer;
+			var selectedLayers = new Array();
+			activeLayer.allLocked = true;
+
+			if (activeLayer.typename == "LayerSet") {
+				// get only layers inside current Group/LayerSet
+				currentGroup = activeLayer;
+				isGroup = true;
+				selectedLayers = addLayerSetContents(activeLayer)
+			} else {
+				// get all selected layers
+				for(var i = 0; i < layersCount; i++) {
+					var layerType = app.activeDocument.layers[i].typename;
+					var layerRef = app.activeDocument.layers[i];
+					if (layerType == "LayerSet") {
+						selectedLayers = addLayerSetContents(layerRef)
+					}
+					if (layerRef.allLocked == true) {
+						selectedLayers.push(layerRef)}
+				}
+			}
+			activeLayer.allLocked = false;
+			return selectedLayers;
 		}
 		catch (e) {
 			alert(e)}
 	}
 
-	/**
-	 * @desc Sorts the layers in the current LayerSet.
-	 * @param {Layers} layers Collection of ArtLayer and LayerSet objects in the current scope.
-	 */
-
+	function selectLayer(layerName, addToSelection) {   
+		// selectLayer("layer 2");
+		// selectLayer("layer 2", true);
+		try {
+			var r = new ActionReference();
+			r.putName(stringIDToTypeID("layer"), layerName);
+			var d = new ActionDescriptor();
+			d.putReference(stringIDToTypeID("null"), r);
+			if (addToSelection == true) d.putEnumerated(stringIDToTypeID("selectionModifier"), 
+											stringIDToTypeID("selectionModifierType"),
+											stringIDToTypeID("addToSelection"));
+			executeAction(stringIDToTypeID("select"), d, DialogModes.NO);
+		}
+		catch (e) { alert(e); throw(e); }
+	}
 	
-	function select_layer(nm, add) {   
-		// 	select_layer("layer 2");
-		
-		// select_layer("layer 1", true);
-
-    try {
-        var r = new ActionReference();
-        r.putName(stringIDToTypeID("layer"), nm);
-        var d = new ActionDescriptor();
-        d.putReference(stringIDToTypeID("null"), r);
-        if (add == true) d.putEnumerated(stringIDToTypeID("selectionModifier"), stringIDToTypeID("selectionModifierType"), stringIDToTypeID("addToSelection"));
-        executeAction(stringIDToTypeID("select"), d, DialogModes.NO);
-		}
-
-    catch (e) { alert(e); throw(e); }
-
-    }
-
 	function sortLayers(layers) {
-		if (layers.length == 0) {
-			alert("No layers selected. Select some layers or select a group for sorting", "No layers to sort");
-			return
-		}
+		/**
+		 * @desc Sorts the layers in the current LayerSet.
+		 * @param {Layers} layers Collection of ArtLayer and LayerSet objects in the current scope.
+		 */
+
 		var activeDoc = app.activeDocument;
 		var all_layers = activeDoc.layers
-		if (all_layers.length == 0) {
-			// Check to see if we need to sort groups in the current layer set.
-			if (layers[index].layers.length > 0) {
-				alert("Getting layers in group", "Sorting Layers");
-				sortLayers(layers[index].layers);
-			}
-			return
-		}
 
+		
 		var layerBuffer = new Array()
 		for (var index = 0; index < layers.length; index++) {
 			if (!layers[index].isBackgroundLayer) {
@@ -108,15 +119,21 @@
 		
 		// Group sorted layers.
 		var GROUP_NAME = 'Sorted Layers';
-		var newGroup = activeDoc.layerSets.add();
-		newGroup.name = GROUP_NAME;
+		if (isGroup) {
+			var targetGroup = currentGroup;
+		} else {
+			var targetGroup = activeDoc.layerSets.add();
+			targetGroup.name = GROUP_NAME;
+		}
 		for (var index = layerBuffer.length - 1; index >= 0; index--) {
-			layerBuffer[index].move(newGroup, ElementPlacement.INSIDE);
+			if (layerBuffer[index].typename == "ArtLayer"){
+				layerBuffer[index].move(targetGroup, ElementPlacement.INSIDE);
+			}
 		}
 		
 		// Select layers.
 		for (var index = 0; index < layerBuffer.length; index++) {
-			select_layer(layerBuffer[index].name, true)
+			selectLayer(layerBuffer[index].name, true)
 		}
 	}
 	
